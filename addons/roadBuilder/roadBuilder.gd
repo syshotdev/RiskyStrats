c6fd8553f93307090c8483af41257980266fee7c
Syshotdev <syshotdev@gmail.com>
2024-01-11 20:28:31 -0800

    Roadbuilder script test working, will modify to actual use

--- addons/roadBuilder/roadBuilder.gd
+++ addons/roadBuilder/roadBuilder.gd
@@ -1,22 +1,39 @@
 @tool
 extends EditorPlugin
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_home"):
-		var editor = get_editor_interface()
-		var nodes = editor.get_selection().get_selected_nodes()
-		var node1 = nodes[0]
-		var node2 = nodes[1]
-		
-		var road = Road.new()
-		
-		var parent = node2
-		
-		road.name = "road"
-		road.node1 = node1
-		road.node2 = node2
-		parent.add_child(road)
-		road.set_owner(parent)
-		
-		print(road.owner)
-		print("Created road successfully")
+func _enter_tree():
+	pass
+
+func _exit_tree():
+	pass
+
+
+func _unhandled_key_input(event):
+	if event is InputEventKey and event.pressed and event.keycode == KEY_KP_ADD:
+		var selected_nodes = get_editor_interface().get_selection().get_selected_nodes()
+		var second_node = selected_nodes[1]
+		addColorRectToNode(second_node)
+
+
+func addColorRectToNode(node : Node):
+	var interface = get_editor_interface()
+	var colorRect := ColorRect.new()
+	colorRect.size = Vector2(100, 100)# Set a default size
+	colorRect.color = Color(1, 0, 0, 1)# Set a default color (Red)
+	colorRect.visible = true# Ensure it's visible
+	colorRect.owner = node
+	
+	# Undo/Redo functionality
+	var undo_redo = get_undo_redo()
+	undo_redo.create_action("Add ColorRect")
+	undo_redo.add_do_method(node, "add_child", colorRect)
+	undo_redo.add_undo_method(node, "remove_child", colorRect)
+	undo_redo.commit_action()
+	
+	interface.get_selection().get_selected_nodes().clear()
+	print(interface.get_selection().get_selected_nodes())
+	interface.get_selection().get_selected_nodes().append(colorRect)
+	print(interface.get_selection().get_selected_nodes())
+	
+	# Debugging
+	print("Added ColorRect to: ", node.name)
